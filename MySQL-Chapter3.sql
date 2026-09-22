dd0296dd416d4e6b6291e1e2172bfb4a6503afde
Faith Coufal <faithcoufal@gmail.com>
2026-09-22 10:40:22 -0500

    Add payment query with filters and sorting
    Added a new SQL query to filter payments by date and amount.

--- MySQL-Chapter3.sql
+++ MySQL-Chapter3.sql
@@ -44,3 +44,19 @@ SELECT title, rental_rate,
 FROM film
 WHERE round(rental_rate * 0.30, 2) >= 0.50
 ORDER BY amount_increased
+
+/*
+Query 4 - Faith Coufal
+ payment amount and date (MM-DD-YYYY)
+ filter by payment date greater than 01-01-2006 inclusively and payment amount greater than 1.00
+ sort the query by payment amount
+ Note: to thoroughly test the query comment out the WHERE clause to verify the filter is working correctly
+*/
+
+SELECT amount, DATE_FORMAT(payment_date, '%m-%d-%y') AS payment_date
+FROM if26faitc_sakila.payment
+
+WHERE amount > 1.00 
+	and payment_date > '2006-01-01' 
+		
+ORDER BY amount;
