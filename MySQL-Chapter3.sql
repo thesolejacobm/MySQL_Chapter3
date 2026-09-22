8e6422b4ed5c95c063b5cfa88d290bb8130f96f3
Jacob Mundt <jaferg01@wsc.edu>
2026-09-22 09:41:22 -0500

    Created header and fixed up query header

--- MySQL-Chapter3.sql
+++ MySQL-Chapter3.sql
@@ -1,5 +1,11 @@
 /*
-Query 1
+My SQL Chapter 3 Assignment
+Jacob Mundt & Faith Coufal
+Started on 9/17/2026
+*/
+
+/*
+Query 1 - Jacob Mundt
      inactive customers
      sorted by name (NOTE: if not stated, all order by clauses are the default ascending)
      Note: to thoroughly test the query, temporarily change the WHERE clause to report active customers
